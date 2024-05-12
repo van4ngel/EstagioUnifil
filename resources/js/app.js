36f504d3cb407f4a4dc7c1879937f15e17a955be
Van4ngel <florrafa94@gmail.com>
2024-05-12 03:02:40 -0300

    Initial components

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,20 @@
-import './bootstrap';
+import { createApp } from 'vue';
+import App from './App.vue';
+
+//PrimeVue imports
+import PrimeVue from 'primevue/config';
+import Button from 'primevue/button';
+import InputText from 'primevue/inputtext';
+
+import 'primevue/resources/themes/aura-dark-blue/theme.css'
+import 'primeicons/primeicons.css'
+
+ 
+const app = createApp({})
+app.use(PrimeVue)
+// PrimeVue load components
+app.component('Button', Button)
+app.component('InputText', InputText)
+
+app.component('app-example', App)
+app.mount('#app')
